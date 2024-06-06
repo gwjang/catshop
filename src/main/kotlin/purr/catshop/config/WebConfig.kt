253d14622738f1f769f628a30baef39ea3b5d0eb
gwjang <zmdlgl@gmail.com>
2024-06-06 16:38:33 +0900

    cors 모두 허용

--- src/main/kotlin/purr/catshop/config/WebConfig.kt
+++ src/main/kotlin/purr/catshop/config/WebConfig.kt
@@ -11,6 +11,5 @@ class WebConfig : WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("*")
             .allowedHeaders("*")
-            .allowCredentials(true)
     }
 }
